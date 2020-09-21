5df93255389c7eabdc80ce06d5d98781db17c265
Fernando Paniagua <fernando.paniagua@gmail.com>
2020-09-21 09:41:23 +0200

    Initial commit

---
+++ Assets/_GameAssets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float fuerza;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 impulso = new Vector3(0, fuerza, 0);
+            rb.AddForce(impulso);
+        }
+    }
+}
